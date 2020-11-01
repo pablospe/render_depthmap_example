e07b0c44879f4fa6b7d27e1e2bb9118a6ca3b369
Pablo Speciale <Pablo.Speciale@microsoft.com>
2020-11-02 00:50:50 +0100

    how to compute depthmap using open3D. It needs a modification of open3D, see README.md

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # open3D_depthmap_example
+
+## How to run the example?
+
+    # creat environment
+    conda env create -f environment_open3d.yml
+    conda activate open3d
+
+    # install package (check platform)
+    pip install open3d-0.11.1-cp38-cp38-win_amd64.whl      (in windows)
+    pip install open3d-0.11.1-cp38-cp38-linux_x86_64.whl   (in linux)
+
+    # run example
+    python main.py
+
+
+#### Capture the screen
+
+![Capture the screen](screenshot_image.png)
+
+#### Capture depth
+
+![Capture depth](screenshot_depth.png)
---
+++ main.py
@@ -0,0 +1,51 @@
+import open3d
+from vizualization import VisOpen3D
+
+
+def main():
+    w = 1024
+    h = 768
+
+    pcd = open3d.io.read_point_cloud("fragment.ply")
+
+    # create window
+    window_visible = False
+    vis = VisOpen3D(width=w, height=h, visible=window_visible)
+
+    # point cloud
+    vis.add_geometry(pcd)
+
+    # update view
+    # vis.update_view_point(intrinsic, extrinsic)
+
+    # save view point to file
+    # vis.save_view_point("view_point.json")
+    vis.load_view_point("view_point.json")
+
+
+    # capture images
+    depth = vis.capture_depth_float_buffer(show=True)
+    image = vis.capture_screen_float_buffer(show=True)
+
+    # save to file
+    vis.capture_screen_image("capture_screen_image.png")
+    vis.capture_depth_image("capture_depth_image.png")
+
+
+    # draw camera
+    if window_visible:
+        vis.load_view_point("view_point.json")
+        intrinsic = vis.get_view_point_intrinsics()
+        extrinsic = vis.get_view_point_extrinsics()
+        vis.draw_camera(intrinsic, extrinsic, scale=0.5, color=[0.8, 0.2, 0.8])
+        # vis.update_view_point(intrinsic, extrinsic)
+
+    if window_visible:
+        vis.load_view_point("view_point.json")
+        vis.run()
+
+    del vis
+
+
+if __name__ == "__main__":
+    main()
