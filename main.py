89fd1da63d8519122831c4a8d25969855bdc944d
Pablo Speciale <Pablo.Speciale@microsoft.com>
2021-02-27 14:18:52 +0100

    Using `render_to_depth_image()` function, trying to see if it gives the correct depth values

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import open3d.visualization.rendering as rendering
 import matplotlib.pyplot as plt
 
 def update_view(camera, intrinsics, extrinsic, width, height):
-    camera.set_projection(intrinsics, 0.1, 100, width, height)
+    z_near = camera.get_near()
+    z_far = camera.get_far()
+
+    camera.set_projection(intrinsics, z_near, z_far, width, height)
     R = extrinsic[:3, :3]
     t = extrinsic[:3, 3]
 
@@ -19,7 +22,7 @@ def main():
 
     # options
     plot_result = True
-    render_depth = False
+    render_depth = True
     if render_depth:
         SHADER = 'depth'
         SUN_LIGHT = False
@@ -49,14 +52,67 @@ def main():
     update_view(render.scene.camera, intrinsic, extrinsic, WIDTH, HEIGHT)
 
     # render image
-    img = render.render_to_image()
-    img = render.render_to_depth_image()
-    o3d.io.write_image(FILENAME, img, 9)
+    if render_depth:
+        # pixels range from 0 (near plane) to 1 (far plane)
+        img = render.render_to_depth_image()
+
+        # conversion: [0,1] -> [near_plane, far_plane]
+        z_near = render.scene.camera.get_near()
+        z_far = render.scene.camera.get_far()
+
+        # img_numpy = (z_far - z_near) * np.asarray(img) + z_near
+
+        # # https://stackoverflow.com/a/62792952
+        # numerator = 2.0 * z_near * z_far
+        # denominator = z_far + z_near - (2.0 * np.asarray(img) - 1.0) * (z_far - z_near)
+        # img_numpy = numerator / denominator
+
+        # conversion inside Open3D:
+        # https://github.com/intel-isl/Open3D/blob/b467935323ca3f9b0d9b2672438bb88e396fd325/cpp/open3d/visualization/visualizer/VisualizerRender.cpp#L410-L413
+        img_numpy = 2.0 * z_near * z_far / (z_far + z_near - (2.0 * np.asarray(img) - 1.0) * (z_far - z_near))
+        img = o3d.geometry.Image(img_numpy)
+
+    else:
+        img = render.render_to_image()
+
+    # o3d.io.write_image(FILENAME, img)
 
     # plot image
     if plot_result:
         plt.imshow(img)
         plt.show()
+
+
+    #
+    # Old Open3D visualizer
+    #
+
+    # create window
+    vis = VisOpen3D(width=WIDTH, height=HEIGHT, visible=False)
+
+    # point cloud
+    vis.add_geometry(pcd)
+
+    # save view point to file
+    # vis.save_view_point("view_point.json")
+    vis.load_view_point("view_point.json")
+
+    # capture images
+    depth = vis.capture_depth_float_buffer(show=False)
+
+    depth_np = np.asarray(depth)
+    img_np = np.asarray(img)
+    depth_np[400,400]   # 3.110481
+    img_np[400,400]     # 0.010031219
+
+    diff = np.asarray(img) - np.asarray(depth)
+    # a = diff
+    # ind = np.unravel_index(np.argmax(a, axis=None), a.shape)
+    # depth_np[ind]
+
+    plt.imshow(diff)
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
